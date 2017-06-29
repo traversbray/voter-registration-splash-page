9524f0f0ecb79b0a28d0f62623fb3d8ed0232697
traversbray <traversbray@gmail.com>
2017-06-29 12:37:28 -0500

    Create registration_page.php

---
+++ registration_page.php
@@ -0,0 +1,73 @@
+<div class="row">
+
+    <!-- left hand column start --> 
+    
+<div class="span4">
+ 
+    
+    <!-- left column image -->
+    <img class="img aligncenter" src="https://www.mainshell.net/wp-content/uploads/2017/06/EngageNevada-300x163.png" alt="EngageNevada" width="329" height="179" align="center" />
+<h2>Voter Registration</h2>
+
+    
+    <div class="hidden-phone">
+    <!-- border image -->
+    <img class="alignnone size-full wp-image-38" src="https://www.mainshell.net/wp-content/uploads/2017/06/rectangle_6.png" alt="rectangle_6" width="530" height="2" />
+    </div>
+    
+<p>Voter registration takes an average of two minutes or less to complete.<p/>
+
+    <div class="hidden-phone">
+    
+<p>Engage Nevada is a non-profit, non-partisan 501(c)(4) organization for civic engagement.<p/>
+
+<p>If you have a valid driver's license of identification card issued by the Nevada DMV, you can register online in Nevada. If you’d like to, select "I have a current NV state identification card or driver's license" and then click "Next Step” and we'll take you to the Nevada Secretary of State's online voter registration system.<p/>
+
+<p>If you don't have a state-issued ID, you can still register to vote using a paper form. Select "I don't have a current NV state identification card or driver's license" and then click "Next Step" and we'll take you to the Nevada Secretary of State website where you can print off your registration application.<p/>
+
+    </div>
+</div>
+<!-- Right hand column that holds the form -->
+    
+<div class="span5">
+
+    
+    <h1>Lets get started with some basic voter information:</h1>
+    <form class="form-horizontal" action="https://www.mainshell.net/index.php/register-complete/" method="post">
+        
+        <div class="form-group"><label for="fname">First Name</label>
+            <input id="fname" class="form-control row" name="fname" type="text" />
+        </div>
+        
+        <div class="form-group"><label for="lname">Last Name</label>
+            <input id="lname" class="form-control row" name="lname" type="text" />
+        </div>
+        
+        <div class="form-group"><label for="email">Email</label>
+            <input id="email" class="form-control row" name="email" type="text" />
+        </div>
+
+        <div class="form-group"><label for="zip">Zip</label>
+            <input id="zip" class="form-control row" name="zip" type="text" />
+        </div>
+
+        Do you have a valid driver's license or identification card issued by the Nevada DMV?
+
+        <div class="checkbox"><label>
+            <input id="" type="checkbox" value="" />I HAVE A CURRENT NV STATE IDENTIFICATION CARD OR DRIVER'S LICENSE</label>
+        </div>
+        <div class="checkbox"><label>
+            <input id="" type="checkbox" value="" />I DO NOT HAVE A CURRENT NV STATE IDENTIFICATION CARD OR DRIVER'S LICENSE</label>
+        </div>
+
+        <button class="btn btn-primary" type="submit">Primary</button>
+
+    </form>
+</div>
+</div>
+&nbsp;
+
+
+<!-- this is here for dynamically editing the footer -->
+
+<footer>Copyright Engage Nevada, Inc. 2015. All Rights Reserved</footer>
